3d7e099ca8709ddec519a78575b563edee65966d
vimfung <vimfung@qq.com>
2019-04-17 17:15:37 +0800

    调整Thread例子脚本

--- Foundation/Sample/Thread-Sample.lua
+++ Foundation/Sample/Thread-Sample.lua
@@ -5,42 +5,23 @@ function Thread_Sample_run()
 
     local t = Thread(function ()
 
-       while(1)
-       do
-           print ("trhead1", a, b);
-           a = 1024;
-           b = "Hahahahahahah";
-
-       end
-
-        -- for i=1,10 do
-        --     print('thread =============', i);
-        -- end
+      while(1)
+      do
+          print ("============ thread 1 handler");
+      end
 
     end);
 
     local t2 = Thread(function ()
 
---        while(1)
---        do
-            print ("thread2", a, b);
-            a = 512;
-            b = "xxxxxx";
---        end
+        print ("============ thread 2 handler");
 
     end);
-
-    local a = 1024;
-    local b = "xxxxxx";
 
     targetThread = t;
 
     t:run();
     t2:run();
-
-    for i=1,10 do
-        print('main =============', i);
-    end
 
 end
 
